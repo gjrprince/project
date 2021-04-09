be98bde347117e084ef49d4219f74cef3a0cdb85
gjrprince <82201826+gjrprince@users.noreply.github.com>
2021-04-09 12:48:57 +0530

    Add files via upload

---
+++ services.php
@@ -0,0 +1,19 @@
+<html>  
+
+<frameset rows="10%,80%">
+
+<frame src="navbar.html"> </frame>
+
+
+<frameset cols="15%,60%,20%"  >
+<!-- name="second" -->
+<frame src="sidebar2.html" > </frame>
+
+<frame src="services1.php"  name="main" > </frame>
+<frame src="adarsh.jpg"> </frame>
+
+
+</frameset>
+
+
+</html>
